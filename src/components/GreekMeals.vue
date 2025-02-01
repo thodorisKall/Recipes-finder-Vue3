f6ce0df284b80b8bd4bf6c085a1a2845d384f4e9
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-02 00:23:27 +0200

    docs: add basic structure for greekmeal component

---
+++ src/components/GreekMeals.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import { greekRecipes } from "../services/services.js"
+
+const recipes = ref([])
+
+onMounted(async () => {
+  try {
+    recipes.value = await greekRecipes()
+  } catch (e) {
+    console.error(`Error getting GreekRecipes ${e.message}`)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h3>Browse through our Greek recipes</h3>
+    <h2>Greek Recipes</h2>
+  </div>
+  <div>
+    <button v-for="recipe in recipes" :key="recipe.idMeal">
+      <p>{{ recipe.strMeal }}</p>
+    </button>
+  </div>
+</template>
