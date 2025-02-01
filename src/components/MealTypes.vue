e7b4ab52b3b5a734da5a707aadc00ae38e76d1e5
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-02 00:26:05 +0200

    docs: add basic structure for mealtypes component

---
+++ src/components/MealTypes.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Browse Through Our Most Popular Recipe</h3>
+    <h2>Browse by Recipe Type</h2>
+  </div>
+  <div>
+    <button v-for="(type, index) in mealTypes" :key="index">
+      <p>{{ type.strCategory }}</p>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import { getMealTypes } from "../services/services.js"
+
+const mealTypes = ref([])
+
+onMounted(async () => {
+  try {
+    mealTypes.value = await getMealTypes()
+    console.log("Fetched meal types:", mealTypes.value)
+  } catch (error) {
+    console.error("Error fetching meal types:", error)
+  }
+})
+</script>
