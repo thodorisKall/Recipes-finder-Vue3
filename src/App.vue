f23691d161741271f2d54dbcbdeaced3b6163d3c
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-02 00:58:53 +0200

    small changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import MealTypes from "./components/MealTypes.vue"
+import MealIngredients from "./components/MealIngredients.vue"
+import Recommended from "./components/RecommendedMeals.vue"
+import GreekMeals from "./components/GreekMeals.vue"
+import Footer from "./components/Footer.vue"
+</script>
 
 <template>
   <h1>Hello World</h1>
+  <MealTypes />
+  <MealIngredients />
+  <Recommended />
+  <GreekMeals />
+  <Footer />
 </template>
