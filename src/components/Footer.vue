53c2a3263cf85303b445aed0c40317290d15e150
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-02 00:57:54 +0200

    docs: add basic structure for footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,68 @@
+<script setup>
+import {
+  Facebook,
+  Instagram,
+  Youtube,
+  Linkedin,
+  Twitter,
+} from "lucide-vue-next"
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <h3>About Us</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed placeat
+          ad tenetur quae fugiat aut rem magnam libero architecto unde.
+        </p>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem, ipsum dolor.</p>
+        <p>Lorem, ipsum.</p>
+      </div>
+    </div>
+    <div>
+      <h3>Quick Links</h3>
+      <ul>
+        <li>About Us</li>
+        <li>Submit Your Recipe</li>
+        <li>Privacy Policy</li>
+        <li>Terms & Conditions</li>
+        <li>Submit Your Photos</li>
+      </ul>
+    </div>
+    <div>
+      <h3>Courses</h3>
+      <ul>
+        <li>Appetizers</li>
+        <li>Breads & Rolls</li>
+        <li>Condiments</li>
+        <li>Desserts</li>
+        <li>Dinners</li>
+      </ul>
+    </div>
+    <div>
+      <h3>Recent Recipes</h3>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h4></h4>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3>Social Links</h3>
+      <ul>
+        <li><Twitter /></li>
+        <li><Linkedin /></li>
+        <li><Facebook /></li>
+        <li><Youtube /></li>
+        <li><Instagram /></li>
+      </ul>
+    </div>
+  </div>
+</template>
