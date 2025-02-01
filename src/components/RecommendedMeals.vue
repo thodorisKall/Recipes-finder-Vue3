295c4c53dc4aa1f23599fad419e967f3cd06b788
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-02 00:58:33 +0200

    docs: add basic structure for recommendedmeals

---
+++ src/components/RecommendedMeals.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { recipes } from "../data/recipes.js"
+</script>
+
+<template>
+  <div>
+    <h3>Explore this collection of our all-time best dinner recipes</h3>
+    <h2>Recommended Recipes</h2>
+  </div>
+  <div v-for="recipe in recipes" :key="recipe.idMeal">
+    <button>
+      <p>{{ recipe.strMeal }}</p>
+    </button>
+  </div>
+</template>
