16332cf124e089b77281b7faa38904b1b202bf6d
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-03 01:35:54 +0200

    docs: create and add basic structure Nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div><img src="../assets/logo.png" alt="" /></div>
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Recipes</li>
+        <li>Chefs</li>
+        <li>Pages</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script></script>
