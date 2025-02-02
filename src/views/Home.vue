fa7330e19dbe96238d4ecf82da28a83d2cd1190f
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-03 01:45:00 +0200

    docs: add other components in Home component and set it for start it route

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MealTypes from "../components/MealTypes.vue"
+import MealIngredients from "../components/MealIngredients.vue"
+import Recommended from "../components/RecommendedMeals.vue"
+import GreekMeals from "../components/GreekMeals.vue"
+import Footer from "../components/Footer.vue"
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <h1>Hello Vue Router</h1>
+    <MealTypes />
+    <MealIngredients />
+    <Recommended />
+    <GreekMeals />
+
+    <router-view />
+  </div>
+</template>
