c6dda3da6645e5c25a4604a2e3c0318afc893739
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-09 21:52:56 +0200

    docs: add one new route for Area view

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Home from "./views/Home.vue"
 import Recipe from "./views/Recipe.vue"
 import Category from "./views/Category.vue"
 import Ingredient from "./views/Ingredient.vue"
+import Area from "./views/Area.vue"
 
 const routes = [
   { path: "/", component: Home },
   { path: "/recipe/:id", component: Recipe },
   { path: "/category/:name", component: Category },
   { path: "/ingredients/:name", component: Ingredient },
+  { path: "/recipes/:area", component: Area },
 ]
 
 const router = createRouter({
