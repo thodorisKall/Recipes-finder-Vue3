50018607880d85764aa0e8a2699ec0f076a3a024
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-02-09 21:50:44 +0200

    feat: add redirect function for greek recipes button at nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,7 +4,7 @@
     <div class="nav_elements">
       <ul>
         <li><router-link to="/">HOME</router-link></li>
-        <li><a href="#greek_recipes">GREEK RECIPES</a></li>
+        <li><router-link to="/recipes/Greek">GREEK RECIPES</router-link></li>
         <li>CHEFS</li>
         <li><a href="#recommended">RECOMMENDED </a></li>
         <li><a href="#footer">CONTACT</a></li>
